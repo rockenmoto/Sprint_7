36d09e968c65b1af49e91829e20da2fed36d32dd
Kirill Belov <rockenmoto@gmail.com>
2024-03-10 15:38:54 -0600

    Актуализация класса Courier

--- courier.py
+++ courier.py
@@ -5,40 +5,35 @@ import requests
 
 
 class Courier:
-    login = ''
-    password = ''
-    first_name = ''
-
     # метод регистрации нового курьера возвращает список из логина и пароля
     # если регистрация не удалась, возвращает пустой список
     def register_new_courier_and_return_login_password(self):
         # создаём список, чтобы метод мог его вернуть
         login_pass = []
 
-        # если регистрация прошла успешно (код ответа 201), добавляем в список логин и пароль курьера
-        if self.create_courier().status_code == 201:
-            login_pass.append(self.login)
-            login_pass.append(self.password)
-            login_pass.append(self.first_name)
-
-        # возвращаем список
-        return login_pass
-
-    def create_courier(self):
-        self.login = self.generate_random_string(10)
-        self.password = self.generate_random_string(10)
-        self.first_name = self.generate_random_string(10)
+        # генерируем логин, пароль и имя курьера
+        login = self.generate_random_string(10)
+        password = self.generate_random_string(10)
+        first_name = self.generate_random_string(10)
 
         # собираем тело запроса
         payload = {
-            "login": self.login,
-            "password": self.password,
-            "firstName": self.first_name
+            "login": login,
+            "password": password,
+            "firstName": first_name
         }
 
         # отправляем запрос на регистрацию курьера и сохраняем ответ в переменную response
         response = requests.post('https://qa-scooter.praktikum-services.ru/api/v1/courier', data=payload)
-        return response
+
+        # если регистрация прошла успешно (код ответа 201), добавляем в список логин и пароль курьера
+        if response.status_code == 201:
+            login_pass.append(login)
+            login_pass.append(password)
+            login_pass.append(first_name)
+
+        # возвращаем список
+        return login_pass
 
     def delete_courier(self, login, password):
         courier_id = self.get_courier_id(login, password)
